053a084059911b669d53711cddad22f96a89b69d
Shingyu Kang <rkdtlseb@naver.com>
2023-10-31 14:09:16 +0900

    first commit

---
+++ src/main/java/com/s/easylog/EasylogApplication.java
@@ -0,0 +1,13 @@
+package com.s.easylog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasylogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasylogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/s/easylog/EasylogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s.easylog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasylogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
